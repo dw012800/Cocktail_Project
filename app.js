52fe8258cd9236808c3e8142d0d86ef6ab8defcf
dw012800 <dw012800@gmail.com>
2024-02-26 17:11:00 -0800

    Functionality complete

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 //API used will be (www.thecocktaildb.com/api/json/v1/1/random.php)
 
 window.onload = () => {
-    // document.querySelector('form').addEventListener('submit', (e) => {
-    //     e.preventDefault()
-
-    // })
     document.querySelector('.btn').addEventListener('click', (event) => {
         event.preventDefault()
    fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php').then(
@@ -24,11 +20,18 @@ window.onload = () => {
         recipe.innerText = json.drinks[0].strInstructions
         const img = document.querySelector("img")
         img.src = json.drinks[0].strDrinkThumb
-        let ingredients = [];
-        for(let i = 17; i < json.drinks[0] - 19; i++){
-            ingredients.push(json.drinks[0][i])
+        let ingredients = []
+        for (let i = 1; i <= 15; i++) {
+          const ingredient = json.drinks[0]["strIngredient" + i]
+          const measure = json.drinks[0]["strMeasure" + i]
+          if (ingredient && measure) {
+            ingredients.push(` ${measure} ${ingredient}`)
+          }
         }
-        console.log(ingredients)
+        const list = document.querySelector("#list")
+        list.innerText = ingredients
+
+
     },
     (err) => {
         console.log(err)
